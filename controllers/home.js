0ad3024647e8dcfffbdc27ca179129af56dcdc4f
Tihomir Vitanov <vitanovtd@gmail.com>
2022-06-06 13:15:54 +0300

    updating hbs viws and controllers

---
+++ controllers/home.js
@@ -0,0 +1,8 @@
+module.exports = {
+
+  async home(req, res) {
+    const cars = await req.storage.getAll();
+    console.log(cars);
+    res.render('index');
+  }
+};
---
+++ index.js
@@ -0,0 +1,44 @@
+// initiliaze and configure Express app
+// initiliaze templating lib
+// create home controller
+// bind routing
+// create layout
+// create data service
+// implement controllers
+
+const express = require('express');
+const hbs = require('express-handlebars');
+
+const carsService = require('./services/cars');
+
+
+const { about } = require('./controllers/about');
+const { create } = require('./controllers/create');
+const { details } = require('./controllers/details');
+const { home } = require('./controllers/home');
+const { notFound } = require('./controllers/notFound');
+
+const app = express();
+
+app.engine(
+  'hbs',
+  hbs.create({
+    extname: '.hbs',
+  }).engine
+);
+
+app.set('view engine', 'hbs');
+
+app.use(express.urlencoded({ extended: true }));
+app.use('/static', express.static('static'));
+app.use(carsService());
+
+
+
+app.get('/', home);
+app.get('/about', about);
+app.get('/create', create);
+app.get('/details/:id', details);
+app.all('*', notFound);
+
+app.listen(3000, () => console.log('Server start on port 3000'));
